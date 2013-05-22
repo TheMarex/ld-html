5cf0183aca4066725701ff66fbf118f7d16274cc
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2013-05-22 15:39:38 +0200

    Added license and readme

--- ld_html.py
+++ ld_html.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
 #
+#  ld-html is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  git-bibisect is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ld-html. If not, see <http://www.gnu.org/licenses/>.
+#
+#  Copyright ld-html 2013: Patrick Niklaus
+#
+#
 # Html Linker
 #
 # Replaces the links generared by pandoc in for of
@@ -12,7 +28,6 @@ import sys
 import os
 import re
 from html.parser import HTMLParser
-
 
 def replace_ids(path, ids):
     f = open(path, "r")
